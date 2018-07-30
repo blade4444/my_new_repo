77b22f81c86b4f4073c16f4bd08ff365445eca55
blade <blademax1996@gmail.com>
2018-07-30 14:07:02 +0300

    my_driver

--- test/test_case.py
+++ test/test_case.py
@@ -1,4 +1,19 @@
 
 
-def test_first(driver):
-    driver.get("https://www.vpnunlimitedapp.com/")
+# def test_first(driver):
+#     driver.get("https://www.vpnunlimitedapp.com/")
+
+import requests
+
+user = 'alexey'
+password = 'styagaylo'
+base_url = 'http://httpbin.org/'
+
+
+def test_my_first_api_test():
+   r = requests.post(base_url + 'post', data={'user': user, 'password': password})
+   assert r.status_code == 200, "Unexpected status code: {}".format(r.status_code)
+   assert r.json()['url'] == base_url + 'post', "Unexpected url: {}".format(r.json()['url'])
+   assert r.json()['form']['user'] == user, "Unexpected user: {}".format(r.json()['form']['user'])
+   assert r.json()['form']['password'] == password, \
+      "Unexpected password: {}".format(r.json()['form']['password'])
