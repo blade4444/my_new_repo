3c9fe19cc3283e9f2d7fe0d4b3fd98ac69468a59
blade <blademax1996@gmail.com>
2018-08-03 15:31:24 +0300

    my_driver

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,26 +1,26 @@
-from page.login_page import AdminPanelLoginPage
-from allure.constants import AttachmentType
-import allure
-
-
-def test_login_to_admin_panel(driver):
-   """
-   === Description:
-   # Open site "http://example.com"
-   # Enter username
-   # Enter password
-   # Submit login
-
-   === Expected result:
-   * Username should be in the title
-   """
-   admin_panel = AdminPanelLoginPage(driver)
-   admin_panel.open()
-   admin_panel.enter_username("blademax1996@gmail.com")
-   admin_panel.enter_password("blade80668481722")
-   admin_panel.submit_login()
-
-
-   with allure.MASTER_HELPER.step('Error'):
-      allure.MASTER_HELPER.attach('screen_shot', driver.get_screenshot_as_png(), type=AttachmentType.PNG)
-
+# from page.login_page import AdminPanelLoginPage
+# from allure.constants import AttachmentType
+# import allure
+#
+#
+# def test_login_to_admin_panel(driver):
+#    """
+#    === Description:
+#    # Open site "http://example.com"
+#    # Enter username
+#    # Enter password
+#    # Submit login
+#
+#    === Expected result:
+#    * Username should be in the title
+#    """
+#    admin_panel = AdminPanelLoginPage(driver)
+#    admin_panel.open()
+#    admin_panel.enter_username("blademax1996@gmail.com")
+#    admin_panel.enter_password("blade80668481722")
+#    admin_panel.submit_login()
+#
+#
+#    with allure.MASTER_HELPER.step('Error'):
+#       allure.MASTER_HELPER.attach('screen_shot', driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+#
